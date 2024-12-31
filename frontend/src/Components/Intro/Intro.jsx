7e03464c397be989eb51a251e3fcc59f79d7ac5e
0nlyssslatts <sid36205@mail.ru>
2024-12-31 08:15:39 +0300

    Intro component added

---
+++ frontend/src/Components/Intro/Intro.jsx
@@ -0,0 +1,12 @@
+
+function Intro(){
+    return(
+        <>
+            <section className="centered">
+                <h1>Добро пожаловать в Weather App</h1>
+            </section>
+        </>
+    )
+}
+
+export default Intro;
