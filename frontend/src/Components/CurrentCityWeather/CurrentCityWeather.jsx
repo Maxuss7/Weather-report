59c3f7618a5c7e10801024a2ec78e2849ea8493f
0nlyssslatts <sid36205@mail.ru>
2025-01-13 10:39:50 +0300

    Current City Weather done

--- frontend/src/Components/CurrentCityWeather/CurrentCityWeather.jsx
+++ frontend/src/Components/CurrentCityWeather/CurrentCityWeather.jsx
@@ -15,21 +15,30 @@ function CurrentCityWeather() {
         <>
             {weather.name && (
                 <section style={currentCityWeatherStyle}>
-                    <h1 style={{ fontWeight: 700, fontSize: "32px" }}>
+                    <h1
+                        style={{
+                            fontWeight: 700,
+                            fontSize: "32px",
+                            marginBottom: 0,
+                        }}
+                    >
                         {weather.name}
                     </h1>
                     <div>
-                        <p>
-                            <span style={{ fontWeight: 700, fontSize: "36px" }}>
+                        <p style={{ marginBottom: 0 }}>
+                            <span style={{ fontWeight: 700, fontSize: "54px" }}>
                                 {weather.main.temp.toFixed(0) == "-0"
                                     ? "0"
                                     : weather.main.temp.toFixed(0)}
                             </span>
-                            <sup style={{ fontSize: "28px" }}>℃</sup>
+                            <sup style={{ fontSize: "40px" }}>℃</sup>
                         </p>
                     </div>
-                    <div>
-                        <p style={{ fontSize: "28px" }}>
+                    <div style={{ fontSize: "22px", textAlign: "center" }}>
+                        <p style={{ marginBottom: 0 }}>
+                            {weather.weather[0].description}
+                        </p>
+                        <p style={{ marginTop: 0 }}>
                             {weather.main.temp_min.toFixed(0) == "-0"
                                 ? "0"
                                 : weather.main.temp_min.toFixed(0)}
