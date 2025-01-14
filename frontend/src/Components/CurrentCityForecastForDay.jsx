f9fe923906f90eea017cd4564c33a77617f92b16
0nlyssslatts <sid36205@mail.ru>
2025-01-14 11:08:19 +0300

    forecast for 5 day added

--- frontend/src/Components/CurrentCityForecastForDay.jsx
+++ frontend/src/Components/CurrentCityForecastForDay.jsx
@@ -1,18 +1,5 @@
 import { useWeather } from "../Context/WeatherProvider";
 
-// const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-// let date = new Date();
-// let dayWeek = days[[6, 0, 1, 2, 3, 4, 5][date.getDay()]];
-// console.log(dayWeek);
-
-// {forecast.list.forEach((element) => {
-//     console.log(
-//         new Date(element.dt * 1000)
-//             .toLocaleTimeString()
-//             .slice(0, 5)
-//     );
-// })}
-
 const currentCityForecastForDayStyle = {
     display: "flex",
     flexDirection: "row",
@@ -24,6 +11,7 @@ const currentCityForecastForDayStyle = {
     margin: "0 20px",
     overflowX: "auto",
     boxShadow: "0px 0px 30px 8px rgba(0, 0, 0, 0.3)",
+    marginBottom: "40px",
 };
 
 const dayItemStyle = {
@@ -69,30 +57,37 @@ function chooseIcon(weather) {
 
 export default function CurrentCityForecast() {
     const { forecast } = useWeather();
-    console.log(forecast);
     return (
-        <section style={currentCityForecastForDayStyle}>
-            {forecast.list &&
-                forecast.list.slice(0, 9).map((day) => {
-                    return (
-                        <div style={dayItemStyle} key={day.dt}>
-                            <div>{day.main.temp.toFixed(0)}°</div>
-                            <div>{chooseIcon(day.weather[0].main)}</div>
-                            <div>{day.wind.speed} m/s</div>
-                            <div>
-                                {new Date(day.dt * 1000)
-                                    .toLocaleTimeString()
-                                    .slice(0, 5) === "00:00"
-                                    ? new Date(day.dt * 1000)
-                                          .toLocaleDateString()
-                                          .slice(0, 5)
-                                    : new Date(day.dt * 1000)
-                                          .toLocaleTimeString()
-                                          .slice(0, 5)}
+        <>
+            <h2 style={{ textAlign: "center" }}>Forecast for 24h</h2>
+            <section style={currentCityForecastForDayStyle}>
+                {forecast.list &&
+                    forecast.list.slice(0, 9).map((day) => {
+                        return (
+                            <div style={dayItemStyle} key={day.dt}>
+                                <div>
+                                    {day.main.temp.toFixed(0) == "-0"
+                                        ? "0"
+                                        : day.main.temp.toFixed(0)}
+                                    °
+                                </div>
+                                <div>{chooseIcon(day.weather[0].main)}</div>
+                                <div>{day.wind.speed} m/s</div>
+                                <div>
+                                    {new Date(day.dt * 1000)
+                                        .toLocaleTimeString()
+                                        .slice(0, 5) === "00:00"
+                                        ? new Date(day.dt * 1000)
+                                              .toLocaleDateString()
+                                              .slice(0, 5)
+                                        : new Date(day.dt * 1000)
+                                              .toLocaleTimeString()
+                                              .slice(0, 5)}
+                                </div>
                             </div>
-                        </div>
-                    );
-                })}
-        </section>
+                        );
+                    })}
+            </section>
+        </>
     );
 }
