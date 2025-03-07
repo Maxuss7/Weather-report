9a35fe47add0703b39ea8f8f0977a4b3d245c266
0nlyssslatts <sid36205@mail.ru>
2025-03-07 08:32:12 +0300

    forecast for 24h weather fixed

--- frontend/src/Components/CurrentCityForecastForDay.jsx
+++ frontend/src/Components/CurrentCityForecastForDay.jsx
@@ -2,13 +2,13 @@ import { useWeather } from "../Context/WeatherProvider";
 import WeatherCardCarousel from "./WeatherCardCarousel.jsx";
 
 export default function CurrentCityForecastForDay() {
-    const { forecast } = useWeather();
+    const { weather } = useWeather();
     return (
         <section>
             <h2 style={{ textAlign: "center" }}>Прогноз на 24 часа</h2>
-            {/* {forecast.list && (
-                <WeatherCardCarousel list={forecast.list.slice(0, 9)} />
-            )} */}
+            {weather.forecast24h[0] && (
+                <WeatherCardCarousel list={weather.forecast24h} />
+            )}
         </section>
     );
 }
