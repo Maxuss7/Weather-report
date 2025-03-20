004f26187dbfd666998af24ddd7934d008c55cc3
0nlyssslatts <sid36205@mail.ru>
2025-03-20 12:33:40 +0300

    swiper added

--- frontend/src/Components/CurrentCityForecastForDay.jsx
+++ frontend/src/Components/CurrentCityForecastForDay.jsx
@@ -1,14 +1,36 @@
 import { useWeather } from "../Context/WeatherProvider";
 import WeatherCardCarousel from "./WeatherCardCarousel.jsx";
+import Swiper from "react-id-swiper";
+import "swiper/swiper-bundle.css";
 
 export default function CurrentCityForecastForDay() {
     const { forecast } = useWeather();
+
+    const params = {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    };
     return (
         <section>
             <h2 style={{ textAlign: "center" }}>Прогноз на 24 часа</h2>
-            {forecast.list && (
-                <WeatherCardCarousel list={forecast.list.slice(0, 9)} />
-            )}
+
+            <Swiper {...params}>
+                {forecast.list && (
+                    <WeatherCardCarousel list={forecast.list.slice(0, 9)} />
+                )}
+            </Swiper>
         </section>
     );
 }
